53cae645de574e2020e4735835991aeedf7f9afb
gness1804 <graham.nessler@gmail.com>
2016-12-24 19:04:46 -0600

    Search input fields draw upon API for calendar and events
    With these commits, the user can now choose from dynamically-generated
    dates and events drawn from the FareHarbor API. When the user clicks the
    "Go" button, they are taken to a calendar of events corresponding to
    their choices. If the user doesn't make any choices, then they are taken
    to a lightframe with all the events for the given company.
    
    Changes input field class names for more clarity and reusability
    
    formats margins
    
    Padding is done
    
    Text align is formatted
    
    Formats background color for input fields
    
    Format background color for button
    
    Format borders for action button
    
    Format border radius and box shadow for button
    
    Format text for button
    
    Format height and width for button
    
    Format margins and padding for button
    
    Format hover state color for button
    
    Finished with SearchInput for now
    
    Adds basic vanilla API request, but not rendering yet
    
    Create field types for both fields and start with API call
    
    Create dropdown for where to input field
    
    WIP -- got dropdown working, but need to get data from API call
    
    Add ability to dynamically change URL for auto-load dropdown API hit based on company
    
    WIP -- can make API call for all items but not for dropdown or calendar
    
    WIP -- still need to get API to return correct data
    
    Allow search input to draw upon API so user can choose event and dates
    
    With these commits, the user can search from dynamically-generated dates
    and events drawn from the FareHarbor API. Clicking the "Go" button sends
    the user to a calendar corresponding to the user's date and event
    choices. If no such choices are specified, the user is sent to a
    lightframe with all events for the given company.
    
    Dropdown populates with event names
    
    Remove icon for left input field if it is of type select
    
    WIP -- working on transferring ID of each event to the calendar input
    
    IT logs the correct ID of target event
    
    Creates default text for left dropdown input
    
    Go button now works dynamically to take user to target event or all events depending on input
    
    Make the right input field into select by default
    
    Correctly store month variable according to month that user specifies in dropdown
    
    Correctly records year based on input in right input field
    
    Where and when input fields now dynamically add content and filter it to URL on search
    
    Capture current month dynamically and display it properly in months dropdown
    
    Captures full year dynamically and uses it for input
    
    Dynamically generates month names several months out
    
    Year now generates dynamically in date dropdown
    
    Dynamically enter current date in URL if user does not specify a date in input field
    
    Footer is now rendering to page
    
    Footer formatting works with correct file name
    
    Format footer background color correctly
    
    Format height and width of footer
    
    Format footer border width
    
    Format the rest of the border properties for footer
    
    Add display property to footer
    
    WIP -- creating structure for generating multiple elements within footer
    
    WIP -- adds option to create multiple text links in footer
    
    Create a filter to only return existing elements for the footer
    
    Text content for each element in footer is showing up
    
    WIP -- moving away from flex
    
    WIP -- reformats footer without flex; adds several links
    
    Centers elements in footer vertically by using height and line-height
    
    WIP -- footer has several elements and now needs social media icons
    
    Finally got facebook image to appear dynamically
    
    Add facebook and twitter icons
    
    WIP -- basic elements are created; need to make footer show and hide
    
    Lightframe shows up when one clicks Book Now in footer
    
    Link in Book Now is now dynamic
    
    Removes hide button from footer
    
    Add google plus icon to footer

---
+++ FHFooter.js
@@ -0,0 +1,96 @@
+var FHFooter = function(obj) {
+  function create(obj) {
+    buildHTML(obj)
+
+  }
+
+  return create(obj)
+}
+
+function buildHTML(obj) {
+
+  var footerStyles = {
+    backgroundColor: obj.footer.footerBackgroundColor || '#88BCF8',
+    height: obj.footer.height || 'auto',
+    width: obj.footer.width || '100vw',
+    display: obj.footer.displayType || 'block',
+    borderTopWidth: obj.footerBorders.footerBorderTopWidth || '1px',
+    borderRightWidth: obj.footerBorders.footerBorderRightWidth || '1px',
+    borderBottomWidth: obj.footerBorders.footerBorderBottomWidth || '1px',
+    borderLeftWidth: obj.footerBorders.footerBorderLeftWidth || '1px',
+    borderTopStyle: obj.footerBorders.footerBorderTopStyle || 'solid',
+    borderRightStyle: obj.footerBorders.footerBorderRightStyle || 'solid',
+    borderBottomStyle: obj.footerBorders.footerBorderBottomStyle || 'solid',
+    borderLeftStyle: obj.footerBorders.footerBorderLeftStyle || 'solid',
+    borderTopColor: obj.footerBorders.footerBorderTopColor || '#88BCF8',
+    borderRightColor: obj.footerBorders.footerBorderRightColor || '#88BCF8',
+    borderBottomColor: obj.footerBorders.footerBorderBottomColor || '#88BCF8',
+    borderLeftColor: obj.footerBorders.footerBorderLeftColor || '#88BCF8',
+  };
+
+  var footer = document.createElement('FOOTER');
+  document.body.appendChild(footer);
+  footer.style.backgroundColor = footerStyles.backgroundColor;
+  footer.style.height = footerStyles.height;
+  footer.style.width = footerStyles.width;
+  footer.style.display = footerStyles.display;
+  footer.style.borderTopWidth = footerStyles.borderTopWidth;
+  footer.style.borderRightWidth = footerStyles.borderRightWidth;
+  footer.style.borderBottomWidth = footerStyles.borderBottomWidth;
+  footer.style.borderLeftWidth = footerStyles.borderLeftWidth;
+  footer.style.borderTopStyle = footerStyles.borderTopStyle;
+  footer.style.borderRightStyle = footerStyles.borderRightStyle;
+  footer.style.borderBottomStyle = footerStyles.borderBottomStyle;
+  footer.style.borderLeftStyle = footerStyles.borderLeftStyle;
+  footer.style.borderTopColor = footerStyles.borderTopColor;
+  footer.style.borderRightColor = footerStyles.borderRightColor;
+  footer.style.borderBottomColor = footerStyles.borderBottomColor;
+  footer.style.borderLeftColor = footerStyles.borderLeftColor;
+
+  var filteredElements = obj.elements.filter(function(element) {
+      return element.doesItExist === true;
+    })
+
+  for (var i = 0; i < filteredElements.length; i++) {
+    var linkWrapper = document.createElement('A');
+    footer.appendChild(linkWrapper);
+    linkWrapper.href = filteredElements[i].linkTo;
+    linkWrapper.style.textDecoration = 'none';
+    linkWrapper.style.color = '#000000';
+    if (filteredElements[i].type === 'textLink') {
+      var text = document.createElement('P');
+      linkWrapper.appendChild(text);
+      var textContent = document.createTextNode(filteredElements[i].textContent);
+      text.appendChild(textContent);
+      text.style.display = 'inline-block';
+      text.style.fontFamily = filteredElements[i].fontName || 'Palatino-Bold, serif';
+      text.style.fontSize = filteredElements[i].fontSize || '16px';
+      text.style.fontWeight = filteredElements[i].fontWeight || 'normal';
+      text.style.marginTop = filteredElements[i].marginTop || '0';
+      text.style.marginRight = filteredElements[i].marginRight || '0';
+      text.style.marginBottom = filteredElements[i].marginBottom || '0';
+      text.style.marginLeft = filteredElements[i].marginLeft || '0';
+      text.style.height = '50px';
+      text.style.lineHeight = '50px';
+      text.style.whiteSpace = 'noWrap';
+    } else if (filteredElements[i].type === 'socialMedia') {
+      var img = document.createElement('IMG');
+      var sources = {
+        facebook: 'images/facebook.png',
+        twitter: 'images/twitter.png',
+        googlePlus: 'images/google-plus3.png',
+      };
+      linkWrapper.appendChild(img);
+      var socialMediaType = filteredElements[i].socialMediaType.toLowerCase();
+      img.src = sources[filteredElements[i].socialMediaType];
+      img.style.verticalAlign = 'middle';
+      img.style.height = filteredElements[i].height || 'auto';
+      img.style.width = filteredElements[i].width || 'auto';
+      img.style.marginTop = filteredElements[i].marginTop || '0';
+      img.style.marginRight = filteredElements[i].marginRight || '0';
+      img.style.marginBottom = filteredElements[i].marginBottom || '0';
+      img.style.marginLeft = filteredElements[i].marginLeft || '0';
+    }
+  }
+
+}
--- FHToolCreator.js
+++ FHToolCreator.js
@@ -1,5 +1,6 @@
 var config = window.FHConfig ? window.FHConfig : null;
 var FHModal = window.FHModal ? window.FHModal : null;
+var FHFooter = window.FHFooter ? window.FHFooter : null;
 var FHSearchInput = window.FHSearchInput ? window.FHSearchInput : null;
 
 var toolCreator = (function() {
@@ -16,13 +17,33 @@ var toolCreator = (function() {
             marginsAndPadding: config.structure.cards.marginsAndPadding.global,
           }
       );
+      case 'Footer':
+        return FHFooter(
+          {
+            footer: config.structure.footer,
+            footerBorders: config.structure.footer.footerBorders,
+            elements: config.structure.footer.elements,
+            footerHideShowTextFont: config.structure.footer.footerHideShowTextFont,
+            shortname: config.shortname,
+          }
+      );
       case 'SearchInput':
         return FHSearchInput(
           {
             inputFieldText: config.structure.inputFields.inputFieldText,
+            inputFieldTypes: config.structure.inputFields.inputFieldTypes,
             inputFieldHeightAndWidth: config.structure.inputFields.inputFieldHeightAndWidth,
             inputFieldIcons: config.structure.inputFields.inputFieldIcons,
             inputFieldBorders: config.structure.inputFields.inputFieldBorders,
+            inputFieldMarginsAndPadding: config.structure.inputFields.inputFieldMarginsAndPadding,
+            inputFieldBackgroundColor: config.structure.inputFields.inputFieldBackgroundColor,
+            buttonsBackgroundColor: config.structure.buttons.buttonsBackgroundColor,
+            buttonsText: config.structure.buttons.buttonsText,
+            buttonsBorders: config.structure.buttons.buttonsBorders,
+            buttonsBellsAndWhistles: config.structure.buttons.buttonsBellsAndWhistles,
+            buttonsHeightAndWidth: config.structure.buttons.buttonsHeightAndWidth,
+            buttonsMarginsAndPadding: config.structure.buttons.buttonsMarginsAndPadding,
+            shortname: config.shortname,
         }
       );
       default:
