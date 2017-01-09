be85d5199939dcfa1278f2c094e8d4f1fd0b275a
gness1804 <graham.nessler@gmail.com>
2017-01-09 13:32:41 -0700

    Additional details link test passes

--- test/search-input-tests.js
+++ test/search-input-tests.js
@@ -246,6 +246,44 @@ test.describe('Search Input',function(){
     driver.quit()
   })
 
-  //try to test disabled and enabling of more details button too
+  it('should change the additional details button link appropriately when the event data changes', function () {
+    var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+    driver.get('http://localhost:8080');
+
+    var detailsButton = driver.findElement({className: 'details-button'});
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+    detailsButton.click();
+
+    var eventField = driver.findElement({className: 'left-field-input'});
+    eventField.click();
+    eventField.sendKeys('Sno');
+    eventField.sendKeys(webdriver.Key.RETURN);
+    eventField.click();
+
+    driver.findElement({className: 'details-button-wrapper'}).then(function (wrapper) {
+      return wrapper.getAttribute('href')
+    }).then(function (link) {
+      assert.strictEqual(link, 'https://demo.fareharbor.com/embeds/book/bodyglove/items/183/calendar/2017/1/?full-items=yes');
+    })
+
+    driver.quit()
+
+  })
 
 })
