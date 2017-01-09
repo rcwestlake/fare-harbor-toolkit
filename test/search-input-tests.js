06784ec33ddefba743cd6a1244b3c4ad78224753
gness1804 <graham.nessler@gmail.com>
2017-01-09 10:13:49 -0700

    Set up test runner with selenium; no real passing tests yet

---
+++ test/search-input-tests.js
@@ -0,0 +1,30 @@
+var assert    = require('assert');
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+
+test.describe('testing ideabox',function(){
+  this.timeout(10000)
+  test.it('should...', function(){
+    var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+    driver.get('http://localhost:8080');
+
+    // var title = driver.findElement({name: 'title'})
+    // var description = driver.findElement({name: 'description'})
+    // title.sendKeys('this is a title').then(()=>{
+    //   return title.getAttribute('value')
+    // }).then((value)=>{
+    //   assert.equal(value, 'this is a title')
+    // })
+    //
+    // description.sendKeys('this is a description').then(()=>{
+    //   return description.getAttribute('value')
+    // }).then((value)=>{
+    //   assert.equal(value, 'this is a description')
+    // })
+
+    driver.quit()
+  })
+})
